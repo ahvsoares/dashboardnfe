6dd5ec1800bbd658b05330d133c8904eac869b5d
Alexandre Henrique Vieira Soares <alexandre.soares@serpro.gov.br>
2024-05-07 13:39:29 -0300

    Adicionada métrica de número de itens vendidos.

--- Dashboard-NFe.py
+++ Dashboard-NFe.py
@@ -7,6 +7,9 @@ import pandas as pd
 import altair as alt
 import streamlit as st
 
+
+## Bases de dados
+
 # Download e descompactação das bases
 URL_NFE = "https://drive.google.com/uc?id=1eTAaOQkO6RT37XHOnR7bP6hBlwu-JLBB"
 FILENAME_NFE = "202401_NFe.zip"
@@ -32,7 +35,8 @@ if not os.path.exists(FILENAME_COORDS):
         zip_ref.extractall()
 # os.remove(FILENAME_COORDS)
 
-# Agora iniciamos a aplicação
+
+## Agora iniciamos a aplicação
 
 st.set_page_config(
     page_title="Dashboard NF-e",
@@ -113,6 +117,82 @@ def criar_matriz_dados_grafico(matriz_valores, labels):
 
     return source
 
+# Função para obter a seção com base no capítulo
+def obter_secao(capitulo):
+    # Dicionário que mapeia os capítulos às suas respectivas seções
+    secoes_por_capitulo = {
+        range(1, 6): 'ANIMAIS VIVOS E PRODUTOS DO REINO ANIMAL',
+        range(6, 15): 'PRODUTOS DO REINO VEGETAL',
+        range(15, 16): 'GORDURAS E ÓLEOS ANIMAIS OU VEGETAIS; PRODUTOS DA SUA DISSOCIAÇÃO; GORDURAS ALIMENTÍCIAS ELABORADAS; CERAS DE ORIGEM ANIMAL OU VEGETAL',
+        range(16, 25): 'PRODUTOS DAS INDÚSTRIAS ALIMENTARES; BEBIDAS, LÍQUIDOS ALCOÓLICOS E VINAGRES; TABACO E SEUS SUCEDÂNEOS MANUFATURADOS',
+        range(25, 28): 'PRODUTOS MINERAIS',
+        range(28, 39): 'PRODUTOS DAS INDÚSTRIAS QUÍMICAS OU DAS INDÚSTRIAS CONEXAS',
+        range(39, 41): 'PLÁSTICO E SUAS OBRAS; BORRACHA E SUAS OBRAS',
+        range(41, 44): 'PELES, COUROS, PELES COM PELO E OBRAS DESTAS MATÉRIAS; ARTIGOS DE CORREEIRO OU DE SELEIRO; ARTIGOS DE VIAGEM, BOLSAS E ARTIGOS SEMELHANTES; OBRAS DE TRIPA',
+        range(44, 47): 'MADEIRA, CARVÃO VEGETAL E OBRAS DE MADEIRA; CORTIÇA E SUAS OBRAS; OBRAS DE ESPARTARIA OU DE CESTARIA',
+        range(47, 50): 'PASTAS DE MADEIRA OU DE OUTRAS MATÉRIAS FIBROSAS CELULÓSICAS; PAPEL OU CARTÃO PARA RECICLAR (DESPERDÍCIOS E APARAS); PAPEL OU CARTÃO E SUAS OBRAS',
+        range(50, 64): 'MATÉRIAS TÊXTEIS E SUAS OBRAS',
+        range(64, 68): 'CALÇADO, CHAPÉUS E ARTIGOS DE USO SEMELHANTE, GUARDA-CHUVAS, GUARDA-SÓIS, BENGALAS, CHICOTES, E SUAS PARTES; PENAS PREPARADAS E SUAS OBRAS; FLORES ARTIFICIAIS; OBRAS DE CABELO',
+        range(68, 71): 'OBRAS DE PEDRA, GESSO, CIMENTO, AMIANTO, MICA OU DE MATÉRIAS SEMELHANTES; PRODUTOS CERÂMICOS; VIDRO E SUAS OBRAS',
+        range(71, 72): 'PÉROLAS NATURAIS OU CULTIVADAS, PEDRAS PRECIOSAS OU SEMIPRECIOSAS E SEMELHANTES, METAIS PRECIOSOS, METAIS FOLHEADOS OU CHAPEADOS DE METAIS PRECIOSOS (PLAQUÊ), E SUAS OBRAS; BIJUTERIAS; MOEDAS',
+        range(72, 84): 'METAIS COMUNS E SUAS OBRAS',
+        range(84, 86): 'MÁQUINAS E APARELHOS, MATERIAL ELÉTRICO, E SUAS PARTES; APARELHOS DE GRAVAÇÃO OU DE REPRODUÇÃO DE SOM, APARELHOS DE GRAVAÇÃO OU DE REPRODUÇÃO DE IMAGENS E DE SOM EM TELEVISÃO, E SUAS PARTES E ACESSÓRIOS',
+        range(86, 90): 'MATERIAL DE TRANSPORTE',
+        range(90, 93): 'INSTRUMENTOS E APARELHOS DE ÓPTICA, DE FOTOGRAFIA, DE CINEMATOGRAFIA, DE MEDIDA, DE CONTROLE OU DE PRECISÃO; INSTRUMENTOS E APARELHOS MÉDICO-CIRÚRGICOS; ARTIGOS DE RELOJOARIA; INSTRUMENTOS MUSICAIS; SUAS PARTES E ACESSÓRIOS',
+        range(93, 94): 'ARMAS E MUNIÇÕES; SUAS PARTES E ACESSÓRIOS',
+        range(94, 97): 'MERCADORIAS E PRODUTOS DIVERSOS',
+        range(97, 98): 'OBJETOS DE ARTE, DE COLEÇÃO E ANTIGUIDADES',
+    }
+
+    capitulo = int(capitulo)
+    for faixa, secao in secoes_por_capitulo.items():
+        if capitulo in faixa:
+            return secao
+    return 'Seção Desconhecida'  # Se o capítulo não estiver mapeado, atribua 'Seção Desconhecida'
+
+@st.cache_data
+def carregar_arquivo_itens_nfe():
+    # Carregando os dados dos itens NF-e
+    df_item = pd.read_csv('202401_NFe_NotaFiscalItem.csv', encoding='ISO-8859-1', sep=';', dtype=str)
+
+    df_item['VALOR TOTAL'] = df_item['VALOR TOTAL'].apply(lambda x: float(x.split()[0].replace(',', '.')))
+    df_item = df_item.astype({'VALOR TOTAL': 'float'})
+
+    # Eliminar as instâncias em que o código NCM informado está claramente errado (valores inexistentes, vazios, etc)
+    df_item = df_item.loc[df_item['CÓDIGO NCM/SH'] != '-1']
+    df_item = df_item.loc[df_item['CÓDIGO NCM/SH'] != '99999999']
+    df_item = df_item.loc[df_item['CÓDIGO NCM/SH'].str.len() >= 7]
+
+    # Criar atributo para o capítulo da NCM
+    df_item['CÓDIGO NCM/SH'] = df_item['CÓDIGO NCM/SH'].apply(lambda x: x.zfill(8))
+    df_item['CAPÍTULO NCM'] = df_item['CÓDIGO NCM/SH'].str.slice(0, 2)
+
+    # Adicionar o novo atributo 'Seção' ao DataFrame
+    df_item['SEÇÃO'] = df_item['CAPÍTULO NCM'].apply(obter_secao)
+
+    # Renomeando campos
+    df_item.rename(columns={'UF EMITENTE': 'sigla_uf'}, inplace=True)
+
+    return df_item
+
+@st.cache_data
+def agregar_valores_mapa_ncm(df_item):
+    # Agrupando por UF e capítulo NCM
+    df_soma_por_uf_ncm = df_item.groupby(['sigla_uf', 'CAPÍTULO NCM'])['VALOR TOTAL'].sum()
+
+    # Selecionado o capítulo de NCM com mais vendas por UF
+    df_maior_soma_por_uf = df_soma_por_uf_ncm.groupby(level='sigla_uf').idxmax()
+
+    # Agrupando por UF e seção
+    df_soma_por_uf_secao = pd.DataFrame(df_item.groupby(['sigla_uf', 'SEÇÃO'], as_index=False, group_keys=False)['VALOR TOTAL'].sum())
+
+    # Selecionado a seção com mais vendas por UF
+    df_maior_soma_por_uf = pd.DataFrame(df_soma_por_uf_secao.groupby(['sigla_uf'], as_index=False).idxmax(numeric_only=True))
+    df_maior_soma_por_uf['CATEGORIA'] = df_maior_soma_por_uf['VALOR TOTAL'].apply(lambda x: df_soma_por_uf_secao.iloc[x]['SEÇÃO'])
+    df_maior_soma_por_uf['VALOR'] = df_maior_soma_por_uf['VALOR TOTAL'].apply(lambda x: df_soma_por_uf_secao.iloc[x]['VALOR TOTAL'])
+
+    return df_maior_soma_por_uf
+
 df_nfe = carregar_arquivo_nfe()
 df_consolidado, labels = agregar_dados_nfe(df_nfe)
 
@@ -122,6 +202,10 @@ df_capitais = carregar_info_capitais()
 matriz_valores = criar_matriz_valores_nfe(df_nfe, labels)
 matriz_grafico_heatmap = criar_matriz_dados_grafico(matriz_valores, labels)
 
+df_item = carregar_arquivo_itens_nfe()
+df_item['QUANTIDADE'] = df_item['QUANTIDADE'].str.replace(',', '.').astype(float)
+
+df_maior_soma_por_uf = agregar_valores_mapa_ncm(df_item)
 
 if "df_nfe" not in st.session_state:
     st.session_state["df_nfe"] = df_nfe
@@ -139,19 +223,35 @@ if "matriz_valores" not in st.session_state:
 if "matriz_grafico_heatmap" not in st.session_state:
     st.session_state["matriz_grafico_heatmap"] = matriz_grafico_heatmap
 
+if "df_item" not in st.session_state:
+    st.session_state["df_item"] = df_item
 
+if "df_maior_soma_por_uf" not in st.session_state:
+    st.session_state["df_maior_soma_por_uf"] = df_maior_soma_por_uf
+
+## Paineis
 
 def formata_numero(valor, prefixo = ''):
-    for unidade in ['', 'mil', 'milhões']:
+    for unidade in ['', 'mil', 'milhões', 'bilhões']:
         if valor < 1_000:
             return f'{prefixo} {valor:.2f} {unidade}'
         valor /= 1_000
-    return f'{prefixo} {valor:.2f} bilhões'
+    return f'{prefixo} {valor:.2f} trilhões'
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.metric('Total de NF-e emitidas no período', formata_numero(df_nfe.shape[0]))
+    st.metric('Valor total de vendas no período', formata_numero(df_nfe['VALOR NOTA FISCAL'].sum(), 'R$'))
 
 with col2:
-    st.metric('Valor total de vendas no período', formata_numero(df_nfe['VALOR NOTA FISCAL'].sum(), 'R$'))
+    st.metric('Total de itens vendidos no período', formata_numero(df_item['QUANTIDADE'].sum()))
+
+
+'''
+Para sexta, podemos colocar ali dashboard:
+ - média de itens por nota
+ - qtde de empresas emissoras
+ - qtde de NCM
+ like 2
+'''
--- pages/4_Categorias_de_Produtos.py
+++ pages/4_Categorias_de_Produtos.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import altair as alt
 import streamlit as st
 import vl_convert as vlc
@@ -11,90 +10,8 @@ st.set_page_config(
 st.markdown("## Categorias de Produtos")
 
 data_geojson = st.session_state["data_geojson"]
-
-# Função para obter a seção com base no capítulo
-def obter_secao(capitulo):
-    # Dicionário que mapeia os capítulos às suas respectivas seções
-    secoes_por_capitulo = {
-        range(1, 6): 'ANIMAIS VIVOS E PRODUTOS DO REINO ANIMAL',
-        range(6, 15): 'PRODUTOS DO REINO VEGETAL',
-        range(15, 16): 'GORDURAS E ÓLEOS ANIMAIS OU VEGETAIS; PRODUTOS DA SUA DISSOCIAÇÃO; GORDURAS ALIMENTÍCIAS ELABORADAS; CERAS DE ORIGEM ANIMAL OU VEGETAL',
-        range(16, 25): 'PRODUTOS DAS INDÚSTRIAS ALIMENTARES; BEBIDAS, LÍQUIDOS ALCOÓLICOS E VINAGRES; TABACO E SEUS SUCEDÂNEOS MANUFATURADOS',
-        range(25, 28): 'PRODUTOS MINERAIS',
-        range(28, 39): 'PRODUTOS DAS INDÚSTRIAS QUÍMICAS OU DAS INDÚSTRIAS CONEXAS',
-        range(39, 41): 'PLÁSTICO E SUAS OBRAS; BORRACHA E SUAS OBRAS',
-        range(41, 44): 'PELES, COUROS, PELES COM PELO E OBRAS DESTAS MATÉRIAS; ARTIGOS DE CORREEIRO OU DE SELEIRO; ARTIGOS DE VIAGEM, BOLSAS E ARTIGOS SEMELHANTES; OBRAS DE TRIPA',
-        range(44, 47): 'MADEIRA, CARVÃO VEGETAL E OBRAS DE MADEIRA; CORTIÇA E SUAS OBRAS; OBRAS DE ESPARTARIA OU DE CESTARIA',
-        range(47, 50): 'PASTAS DE MADEIRA OU DE OUTRAS MATÉRIAS FIBROSAS CELULÓSICAS; PAPEL OU CARTÃO PARA RECICLAR (DESPERDÍCIOS E APARAS); PAPEL OU CARTÃO E SUAS OBRAS',
-        range(50, 64): 'MATÉRIAS TÊXTEIS E SUAS OBRAS',
-        range(64, 68): 'CALÇADO, CHAPÉUS E ARTIGOS DE USO SEMELHANTE, GUARDA-CHUVAS, GUARDA-SÓIS, BENGALAS, CHICOTES, E SUAS PARTES; PENAS PREPARADAS E SUAS OBRAS; FLORES ARTIFICIAIS; OBRAS DE CABELO',
-        range(68, 71): 'OBRAS DE PEDRA, GESSO, CIMENTO, AMIANTO, MICA OU DE MATÉRIAS SEMELHANTES; PRODUTOS CERÂMICOS; VIDRO E SUAS OBRAS',
-        range(71, 72): 'PÉROLAS NATURAIS OU CULTIVADAS, PEDRAS PRECIOSAS OU SEMIPRECIOSAS E SEMELHANTES, METAIS PRECIOSOS, METAIS FOLHEADOS OU CHAPEADOS DE METAIS PRECIOSOS (PLAQUÊ), E SUAS OBRAS; BIJUTERIAS; MOEDAS',
-        range(72, 84): 'METAIS COMUNS E SUAS OBRAS',
-        range(84, 86): 'MÁQUINAS E APARELHOS, MATERIAL ELÉTRICO, E SUAS PARTES; APARELHOS DE GRAVAÇÃO OU DE REPRODUÇÃO DE SOM, APARELHOS DE GRAVAÇÃO OU DE REPRODUÇÃO DE IMAGENS E DE SOM EM TELEVISÃO, E SUAS PARTES E ACESSÓRIOS',
-        range(86, 90): 'MATERIAL DE TRANSPORTE',
-        range(90, 93): 'INSTRUMENTOS E APARELHOS DE ÓPTICA, DE FOTOGRAFIA, DE CINEMATOGRAFIA, DE MEDIDA, DE CONTROLE OU DE PRECISÃO; INSTRUMENTOS E APARELHOS MÉDICO-CIRÚRGICOS; ARTIGOS DE RELOJOARIA; INSTRUMENTOS MUSICAIS; SUAS PARTES E ACESSÓRIOS',
-        range(93, 94): 'ARMAS E MUNIÇÕES; SUAS PARTES E ACESSÓRIOS',
-        range(94, 97): 'MERCADORIAS E PRODUTOS DIVERSOS',
-        range(97, 98): 'OBJETOS DE ARTE, DE COLEÇÃO E ANTIGUIDADES',
-    }
-
-    capitulo = int(capitulo)
-    for faixa, secao in secoes_por_capitulo.items():
-        if capitulo in faixa:
-            return secao
-    return 'Seção Desconhecida'  # Se o capítulo não estiver mapeado, atribua 'Seção Desconhecida'
-
-@st.cache_data
-def carregar_arquivo_itens_nfe():
-    # Carregando os dados dos itens NF-e
-    df_item = pd.read_csv('202401_NFe_NotaFiscalItem.csv', encoding='ISO-8859-1', sep=';', dtype=str)
-
-    df_item['VALOR TOTAL'] = df_item['VALOR TOTAL'].apply(lambda x: float(x.split()[0].replace(',', '.')))
-    df_item = df_item.astype({'VALOR TOTAL': 'float'})
-
-    # Eliminar as instâncias em que o código NCM informado está claramente errado (valores inexistentes, vazios, etc)
-    df_item = df_item.loc[df_item['CÓDIGO NCM/SH'] != '-1']
-    df_item = df_item.loc[df_item['CÓDIGO NCM/SH'] != '99999999']
-    df_item = df_item.loc[df_item['CÓDIGO NCM/SH'].str.len() >= 7]
-
-    # Criar atributo para o capítulo da NCM
-    df_item['CÓDIGO NCM/SH'] = df_item['CÓDIGO NCM/SH'].apply(lambda x: x.zfill(8))
-    df_item['CAPÍTULO NCM'] = df_item['CÓDIGO NCM/SH'].str.slice(0, 2)
-
-    # Adicionar o novo atributo 'Seção' ao DataFrame
-    df_item['SEÇÃO'] = df_item['CAPÍTULO NCM'].apply(obter_secao)
-
-    # Renomeando campos
-    df_item.rename(columns={'UF EMITENTE': 'sigla_uf'}, inplace=True)
-
-    return df_item
-
-if "df_item" not in st.session_state:
-    df_item = carregar_arquivo_itens_nfe()
-    st.session_state["df_item"] = df_item
-else:
-    df_item = st.session_state["df_item"]
-
-@st.cache_data
-def agregar_valores_mapa_ncm(df_item):
-    # Agrupando por UF e capítulo NCM
-    df_soma_por_uf_ncm = df_item.groupby(['sigla_uf', 'CAPÍTULO NCM'])['VALOR TOTAL'].sum()
-
-    # Selecionado o capítulo de NCM com mais vendas por UF
-    df_maior_soma_por_uf = df_soma_por_uf_ncm.groupby(level='sigla_uf').idxmax()
-
-    # Agrupando por UF e seção
-    df_soma_por_uf_secao = pd.DataFrame(df_item.groupby(['sigla_uf', 'SEÇÃO'], as_index=False, group_keys=False)['VALOR TOTAL'].sum())
-
-    # Selecionado a seção com mais vendas por UF
-    df_maior_soma_por_uf = pd.DataFrame(df_soma_por_uf_secao.groupby(['sigla_uf'], as_index=False).idxmax(numeric_only=True))
-    df_maior_soma_por_uf['CATEGORIA'] = df_maior_soma_por_uf['VALOR TOTAL'].apply(lambda x: df_soma_por_uf_secao.iloc[x]['SEÇÃO'])
-    df_maior_soma_por_uf['VALOR'] = df_maior_soma_por_uf['VALOR TOTAL'].apply(lambda x: df_soma_por_uf_secao.iloc[x]['VALOR TOTAL'])
-
-    return df_maior_soma_por_uf
-
-df_maior_soma_por_uf = agregar_valores_mapa_ncm(df_item)
+df_item = st.session_state["df_item"]
+df_maior_soma_por_uf = st.session_state["df_maior_soma_por_uf"]
 
 tab1, tab2 = st.tabs(['Mapa Produto mais Vendido por UF', 'Sumarização UF/Produto'])
 
